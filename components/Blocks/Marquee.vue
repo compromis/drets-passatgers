541f14647450635e6bc968a90e2052fc31f63c15
Guillem <guillem@disedit.com>
2024-12-18 18:43:54 +0100

    minor tweak

--- components/Blocks/Marquee.vue
+++ components/Blocks/Marquee.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="sticky bottom-0 p-site z-[1000]">
-    <aside class="border-[3px] rounded-xl flex overflow-clip bg-yellow">
+    <aside class="border-2 rounded-xl flex overflow-clip bg-yellow">
       <Vue3Marquee class="marquee self-center p-3 text-md font-extrabold flex h-full" :duration="50">
         <div v-for="line in $tm('marquee.questions')" class="mx-4">
           {{ $rt(line) }}
         </div>
       </Vue3Marquee>
-      <div class="cta flex border-l-[3px]">
+      <div class="cta flex border-l-2">
         <a href="#form" class="p-site bg-red h-full flex gap-2 items-center text-white font-extrabold text-md hover:bg-black hover:text-white focus-yellow hover:no-underline">
           <Icon name="wpf:paperplane" />
           {{ $t('marquee.cta') }}
